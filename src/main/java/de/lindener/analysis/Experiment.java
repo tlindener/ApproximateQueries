cc77f8fdabcb381d2d1b4c09a5c5b6bfdf13f55e
tlindener <tobias.lindener@outlook.com>
2018-04-20 11:54:10 +0200

    Add experiment class Add new dependencies

---
+++ src/main/java/de/lindener/analysis/Experiment.java
@@ -0,0 +1,11 @@
+package de.lindener.analysis;
+
+import java.time.LocalDateTime;
+
+public class Experiment {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int runtime;
+    private String settings;
+    private String resultPath;
+}
